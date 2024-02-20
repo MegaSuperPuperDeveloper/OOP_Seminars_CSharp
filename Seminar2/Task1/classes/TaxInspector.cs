83d3ec99d2f3cbadec444922622d081f5b5bba73
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-20 14:02:06 +0300

    End of Seminar

--- Seminar2/Task1/classes/TaxInspector.cs
+++ Seminar2/Task1/classes/TaxInspector.cs
@@ -1,6 +1,38 @@
-﻿namespace Seminar2.Task1;
+﻿using Seminar2.Task1;
 
-public class TaxInspector
+public class TaxInspector : iActorBehaviour
 {
-    
+    private String name;
+    private bool isTakeOrder;
+    private bool isMakeOrder;
+
+    public TaxInspector()
+    {
+        this.name = "Налоговый инспектор";
+    }
+
+    bool iActorBehaviour.isTakeOrder()
+    {
+        return isTakeOrder;
+    }
+
+    bool iActorBehaviour.isMakeOrder()
+    {
+        return isMakeOrder;
+    }
+
+    public void setTakeOrder(bool isCreated)
+    {
+        isTakeOrder = isCreated;
+    }
+
+    public void setMakeOrder(bool isCreated)
+    {
+        isMakeOrder = isCreated;
+    }
+
+    public Actor getActor()
+    {
+        return new OrdinaryClient("Тайный клиент");
+    }
 }
