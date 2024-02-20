ee959dde3961ee99522b131208c886f7b489d1d4
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-20 14:00:36 +0300

    End of Seminar

--- Seminar2/Task1/interfaces/iQueueBehaviour.cs
+++ Seminar2/Task1/interfaces/iQueueBehaviour.cs
@@ -1,6 +1,9 @@
-﻿namespace Seminar2.Task1;
+﻿using Seminar2.Task1;
 
-public interface iQueueBehaviour
+public interface iQueueBehaviour // Интерфейс для взаимодействия внутри магазина
 {
-    
+    void TakeInQueue(iActorBehaviour actor);
+    void ReleaseFromQueue();
+    void TakeOrder();
+    void GiveOrder();
 }
