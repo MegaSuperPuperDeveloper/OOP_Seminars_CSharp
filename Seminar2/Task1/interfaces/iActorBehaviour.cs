1bd24c312acb3465d5512b4a47a9683c9b868818
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-20 13:59:19 +0300

    End of Seminar

--- Seminar2/Task1/interfaces/iActorBehaviour.cs
+++ Seminar2/Task1/interfaces/iActorBehaviour.cs
@@ -1,6 +1,9 @@
-﻿namespace Seminar2.Task1.interfaces;
+﻿using Seminar2.Task1;
 
-public interface iActorBehaviour
-{
-    
+public interface iActorBehaviour {
+    public abstract bool isTakeOrder();
+    public abstract bool isMakeOrder();
+    public abstract void setTakeOrder(bool isCreated);
+    public abstract void setMakeOrder(bool isCreated);
+    public abstract Actor getActor();
 }
