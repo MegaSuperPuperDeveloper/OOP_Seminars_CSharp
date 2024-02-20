8f9f544451af6311505bd027c52da7837b5f1003
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-20 14:02:14 +0300

    End of Seminar

--- Seminar2/Task1/classes/SpecialClient.cs
+++ Seminar2/Task1/classes/SpecialClient.cs
@@ -1,6 +1,48 @@
-﻿namespace Seminar2.Task1;
+﻿using Seminar2.Task1;
 
-public class SpecialClient
+public class SpecialClient : Actor // "Вип" клиент
 {
+
+    public SpecialClient(string name, int idVip) : base(name)
+    {
+        this.idVip = idVip;
+    }
+    
+    private int idVip;
+    
+    public bool isMakeOrder()
+    {
+        return isMakeOrder();
+    }
+
+    public bool isTakeOrder()
+    {
+        return isTakeOrder();
+    }
+
+    public void setMakeOrder(bool isCreated)
+    {
+        base.isMakeOrder = isCreated;
+    }
+
+    public void setTakeOrder(bool isCreated)
+    {
+        base.isTakeOrder = isCreated;
+    }
+
+    public Actor getActor()
+    {
+        return this;
+    }
+
+    public override string getName()
+    {
+        return name;
+    }
+
+    public override void setName(string name)
+    {
+        base.name = name;
+    }
     
 }
