4a2fcba5fdb77283a56a473f26ef37a67ac2b934
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-20 14:01:38 +0300

    End of Seminar

--- Seminar2/Task1/classes/Actor.cs
+++ Seminar2/Task1/classes/Actor.cs
@@ -1,6 +1,40 @@
-﻿namespace Seminar2.Task1.classes;
+﻿namespace Seminar2.Task1;
 
-public class Actor
+public abstract class Actor : iActorBehaviour // Шаблон для разных видов клиентов
 {
-    
+    protected String name;
+    protected internal bool isTakeOrder;
+    protected internal bool isMakeOrder;
+
+    public Actor(String name) {
+        this.name = name;
+    }
+
+    public abstract String getName();
+    public abstract void setName(String name);
+
+    bool iActorBehaviour.isTakeOrder()
+    {
+        return isTakeOrder;
+    }
+
+    bool iActorBehaviour.isMakeOrder()
+    {
+        return isMakeOrder;
+    }
+
+    public void setTakeOrder(bool isCreated)
+    {
+        isTakeOrder = isCreated;
+    }
+
+    public void setMakeOrder(bool isCreated)
+    {
+        isMakeOrder = isCreated;
+    }
+
+    public Actor getActor()
+    {
+        return this;
+    }
 }
