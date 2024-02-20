1eefd2797b28b79396334aea2ccc4f031bfb1357
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-20 13:59:29 +0300

    End of Seminar

--- Seminar2/Task1/interfaces/iMarketBehaviour.cs
+++ Seminar2/Task1/interfaces/iMarketBehaviour.cs
@@ -1,6 +1,8 @@
-﻿namespace Seminar2.Task1;
+﻿using Seminar2.Task1;
 
-public interface iMarketBehaviour
+public interface iMarketBehaviour // Интерфейс для входа и выхода из магазина
 {
-    
+    void AcceptToMarket(iActorBehaviour actor);
+    void ReleaseFromMarket(List<iActorBehaviour> actor);
+    void update();
 }
