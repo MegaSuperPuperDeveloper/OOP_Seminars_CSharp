534dd1a1f9c4aff611eadb909e048d333e669059
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-20 14:02:47 +0300

    End of Seminar

--- Seminar2/Task1/classes/OrinaryClient.cs
+++ Seminar2/Task1/classes/OrinaryClient.cs
@@ -1,6 +1,42 @@
-﻿namespace Seminar2.Task1;
+﻿using Seminar2.Task1;
 
-public class OrinaryClient
+public class OrdinaryClient : Actor // Дефолт клиент
 {
+
+    public OrdinaryClient(string name) : base(name) {}
     
+    public bool isMakeOrder()
+    {
+        return base.isMakeOrder;
+    }
+
+    public bool isTakeOrder()
+    {
+        return base.isTakeOrder;
+    }
+
+    public void setMakeOrder(bool isCreated)
+    {
+        base.isMakeOrder = isCreated;
+    }
+
+    public void setTakeOrder(bool isCreated)
+    {
+        base.isTakeOrder = isCreated;
+    }
+
+    public Actor getActor()
+    {
+        return this;
+    }
+
+    public override string getName()
+    {
+        return name;
+    }
+
+    public override void setName(string name)
+    {
+        base.name = name;
+    }
 }
